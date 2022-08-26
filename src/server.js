17c8b2af18ed31c0ad89a5dc6aeddf19205b45bb
Trần Hoàng Long <alessandrotran1209@gmail.com>
2022-08-26 14:40:44 +0700

    Update project name

--- src/server.js
+++ src/server.js
@@ -10,10 +10,8 @@ const express = require("express");
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static("./dist/angular-heroku"));
+app.use(express.static("./dist/QLCV"));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/angular-heroku/" })
-);
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/QLCV/" }));
 
 app.listen(process.env.PORT || 8080);
