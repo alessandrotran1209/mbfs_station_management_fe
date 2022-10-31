246b9f0397ec5a23002062d94323be79882d52b1
Trần Hoàng Long <alessandrotran1209@gmail.com>
2022-10-31 11:01:23 +0700

    Adding detailed report page

--- src/app/report/report-overview/report-overview.component.ts
+++ src/app/report/report-overview/report-overview.component.ts
@@ -1,7 +1,47 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
+import { ApiService } from 'src/app/services/api.service';
+import { OperationApiService } from 'src/app/services/operation-api.service';
+import { Operation } from 'src/app/utils/operation';
 
+import {
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+} from '@angular/material/core';
+import { MatDatepicker } from '@angular/material/datepicker';
+
+// Depending on whether rollup is used, moment needs to be imported differently.
+// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
+// syntax. However, rollup creates a synthetic default module and we thus need to import it using
+// the `default as` syntax.
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import { default as _rollupMoment, Moment } from 'moment';
+import { ExportService } from 'src/app/services/export.service';
+
+const moment = _rollupMoment || _moment;
+
+// See the Moment.js docs for the meaning of these formats:
+// https://momentjs.com/docs/#/displaying/format/
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 export interface PeriodicElement {
   position: number;
   code: string;
@@ -9,30 +49,6 @@ export interface PeriodicElement {
   date: string;
   status: number;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {
-    position: 1,
-    code: 'TB_DHG_BACH_DANG',
-    work: 'Xử lý lỗi thiết bị Tủ 2G, 3G và 4G',
-    date: '22/07/2022',
-    status: 1,
-  },
-  {
-    position: 2,
-    code: 'TB_DHG_BACH_DANG',
-    work: 'Xử lý lỗi  RRU, lỗi dây quang, lỗi Feeder, Jumper và anten 2G, 3G, 4G',
-    date: '22/07/2022',
-    status: 0,
-  },
-  {
-    position: 3,
-    code: 'TB_DHG_BACH_DANG',
-    work: 'Phối hợp các bên Điện lực, chiếu sáng, quản lý đô thị để thực hiện các công việc liên quan đến cáp trên cột, trong cống bể…',
-    date: '22/07/2022',
-    status: 1,
-  },
-];
 
 interface Food {
   value: string;
@@ -43,12 +59,51 @@ interface Food {
   selector: 'app-report-overview',
   templateUrl: './report-overview.component.html',
   styleUrls: ['./report-overview.component.scss'],
+  providers: [
+    // `MomentDateAdapter` can be automatically provided by importing `MomentDateModule` in your
+    // application's root module. We provide it at the component level here, due to limitations of
+    // our example generation script.
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+
+    { provide: MAT_DATE_FORMATS, useValue: MY_FORMATS },
+  ],
 })
 export class ReportOverviewComponent implements OnInit {
-  constructor(public dialog: MatDialog, public fb: FormBuilder) {}
+  constructor(
+    public dialog: MatDialog,
+    public fb: FormBuilder,
+    private apiService: ApiService,
+    public operationApiService: OperationApiService,
+    public datepipe: DatePipe,
+    private exportService: ExportService
+  ) {}
+  total_completed_operation: 0;
+  total_uncompleted_operation: 0;
+  operation_list: any[] = [];
+  panelOpenState = false;
 
   ngOnInit(): void {
     this.reactiveForm();
+    var operation = new Operation();
+    for (var i: number = 1; i < operation.getLength(); i++) {
+      var option = operation.getOperation(String(i));
+      this.operation_list.push({
+        value: option.value,
+        viewValue: option.viewValue,
+      });
+    }
+
+    this.apiService.getStatistics().subscribe((response: any) => {
+      this.total_completed_operation = response.data.total_completed_operation;
+      this.total_uncompleted_operation =
+        response.data.total_uncompleted_operation;
+    });
+
+    this.getServerData(null, false);
   }
 
   searchForm!: FormGroup;
@@ -56,125 +111,20 @@ export class ReportOverviewComponent implements OnInit {
   reactiveForm() {
     this.searchForm = this.fb.group({
       code: [''],
-      work: [''],
+      month: [''],
+      work_code: [''],
       status: [''],
-      province: [''],
-      district: [''],
     });
   }
 
-  displayedColumns: string[] = ['position', 'code', 'work', 'date', 'status'];
-  dataSource = ELEMENT_DATA;
-
-  foods: Food[] = [
-    {
-      value: 'Xử lý lỗi thiết bị Tủ 2G, 3G và 4G',
-      viewValue: 'Xử lý lỗi thiết bị Tủ 2G, 3G và 4G',
-    },
-    {
-      value:
-        'Xử lý lỗi  RRU, lỗi dây quang, lỗi Feeder, Jumper và anten 2G, 3G, 4G',
-      viewValue:
-        'Xử lý lỗi  RRU, lỗi dây quang, lỗi Feeder, Jumper và anten 2G, 3G, 4G',
-    },
-    {
-      value: 'Xử lý lỗi thiết bị truyền dẫn Viba',
-      viewValue: 'Xử lý lỗi thiết bị truyền dẫn Viba',
-    },
-    {
-      value:
-        'Xử lý lỗi thiết bị truyền dẫn Quang trong trạm: dây nhảy, CSG, ODF, Modul quang…',
-      viewValue:
-        'Xử lý lỗi thiết bị truyền dẫn Quang trong trạm: dây nhảy, CSG, ODF, Modul quang…',
-    },
-    {
-      value: 'Xử lý lỗi các thiết bị phụ trợ: Cảnh báo ngoài, tủ nguồn,…',
-      viewValue: 'Xử lý lỗi các thiết bị phụ trợ: Cảnh báo ngoài, tủ nguồn,…',
-    },
-    {
-      value: 'Thực hiện nâng-hạ cấp cấu hình thiết bị theo yêu cầu của MBF',
-      viewValue: 'Thực hiện nâng-hạ cấp cấu hình thiết bị theo yêu cầu của MBF',
-    },
-    {
-      value:
-        'Đo kiểm xác định điểm đứt cáp quang, triển khai kéo cáp, hàn nối măng xông và treo măng xông',
-      viewValue:
-        'Đo kiểm xác định điểm đứt cáp quang, triển khai kéo cáp, hàn nối măng xông và treo măng xông',
-    },
-    {
-      value: 'Đo kiểm và hiệu chỉnh trống Viba',
-      viewValue: 'Đo kiểm và hiệu chỉnh trống Viba',
-    },
-    {
-      value: 'Kiểm tra, thay thế dây IF viba',
-      viewValue: 'Kiểm tra, thay thế dây IF viba',
-    },
-    {
-      value: 'Đo kiểm, xử lý suy hao cáp..',
-      viewValue: 'Đo kiểm, xử lý suy hao cáp..',
-    },
-    {
-      value: 'Triển khai chạy máy phát điện di động',
-      viewValue: 'Triển khai chạy máy phát điện di động',
-    },
-    {
-      value: 'Triển khai chạy máy phát điện cố định',
-      viewValue: 'Triển khai chạy máy phát điện cố định',
-    },
-    {
-      value: 'Vệ sinh công nghiệp nhà trạm',
-      viewValue: 'Vệ sinh công nghiệp nhà trạm',
-    },
-    {
-      value: 'Tiếp nhận, liên hệ khách hàng theo thông tin PA',
-      viewValue: 'Tiếp nhận, liên hệ khách hàng theo thông tin PA',
-    },
-    {
-      value: 'Đến hiện trường đo kiểm và phối hợp xác định nguyên nhân',
-      viewValue: 'Đến hiện trường đo kiểm và phối hợp xác định nguyên nhân',
-    },
-    {
-      value:
-        'Phối hợp hiệu chỉnh anten, lắp đặt và điều chuyển Smallcell, Repeater',
-      viewValue:
-        'Phối hợp hiệu chỉnh anten, lắp đặt và điều chuyển Smallcell, Repeater',
-    },
-    {
-      value: 'Kiểm tra, cập nhật tuyến cáp định kì',
-      viewValue: 'Kiểm tra, cập nhật tuyến cáp định kì',
-    },
-    {
-      value: 'Bảo dưỡng, căng chùng tuyến cáp nguy cơ mất an toàn..',
-      viewValue: 'Bảo dưỡng, căng chùng tuyến cáp nguy cơ mất an toàn..',
-    },
-    {
-      value:
-        'Phối hợp các bên Điện lực, chiếu sáng, quản lý đô thị để thực hiện các công việc liên quan đến cáp trên cột, trong cống bể…',
-      viewValue:
-        'Phối hợp các bên Điện lực, chiếu sáng, quản lý đô thị để thực hiện các công việc liên quan đến cáp trên cột, trong cống bể…',
-    },
-    {
-      value:
-        'Hỗ trợ các đối  tác ra vào trạm, giám sát công việc tại trạm: bảo dưỡng, lắp đặt, thay thế thiết bị, hạ tầng nhà trạm..',
-      viewValue:
-        'Hỗ trợ các đối  tác ra vào trạm, giám sát công việc tại trạm: bảo dưỡng, lắp đặt, thay thế thiết bị, hạ tầng nhà trạm..',
-    },
-    {
-      value: 'Phối hợp giám sát tuyến cáp Quang',
-      viewValue: 'Phối hợp giám sát tuyến cáp Quang',
-    },
-    {
-      value: 'Tiếp nhận và đóng ticket sự cố trạm quản lý',
-      viewValue: 'Tiếp nhận và đóng ticket sự cố trạm quản lý',
-    },
-    {
-      value: 'Hồ sơ thanh toán tiền điện, thuê nhà trạm',
-      viewValue: 'Hồ sơ thanh toán tiền điện, thuê nhà trạm',
-    },
-    {
-      value: 'Công việc khác ….',
-      viewValue: 'Công việc khác ….',
-    },
+  displayedColumns: string[] = [
+    'position',
+    'code',
+    'operator',
+    'work',
+    'start_date',
+    'end_date',
+    'status',
   ];
 
   status_list: Food[] = [
@@ -187,43 +137,177 @@ export class ReportOverviewComponent implements OnInit {
       viewValue: 'Hoàn thành',
     },
   ];
-  province_list: Food[] = [
-    { value: 'HN', viewValue: 'Hà Nội' },
-    { value: 'TB', viewValue: 'Thái Bình' },
-  ];
-  districts_list: any[] = [
-    {
-      province: 'HN',
-      districts: [
-        { value: 'BD', viewValue: 'Ba Đình' },
-        { value: 'CG', viewValue: 'Cầu Giấy' },
-      ],
-    },
-    {
-      province: 'TB',
-      districts: [
-        { value: 'DH', viewValue: 'Đông Hưng' },
-        { value: 'HH', viewValue: 'Hưng Hà' },
-      ],
-    },
-  ];
-
-  districts: any[] = [];
-
-  updateProvince(event: any): void {
-    for (var d of this.districts_list) {
-      if (d.province == event) {
-        this.districts = d.districts;
-        break;
-      }
-    }
-  }
 
   clearForm(): void {
-    this.searchForm.reset();
+    this.reactiveForm();
+    this.date = new FormControl(moment());
+    this.getServerData(null, false);
   }
 
-  onFormSubmit(): void {}
+  onFormSubmit(): void {
+    this.getServerData(null, true);
+  }
 
   maxDate: Date = new Date();
+
+  pageEvent: PageEvent;
+  datasource: any = [];
+  pageIndex: number;
+  pageSize: number = 10;
+  length: number;
+  total: number;
+  public getServerData(event?: PageEvent, isSearching?: boolean) {
+    if (isSearching) {
+      let page = 1;
+      if (event != null) {
+        page = event.pageIndex + 1;
+      }
+
+      const fromDate = this.date.value.add(1, 'M');
+      var startOfMonth = `1/${fromDate.month()}/${fromDate.year()}`;
+
+      const toDate = fromDate.add(1, 'M');
+      var startOfNextMonth = `1/${toDate.month()}/${toDate.year()}`;
+
+      this.date.setValue(this.date.value.subtract(2, 'M'));
+
+      let code = this.searchForm.value.code;
+      const work_code = this.searchForm.value.work_code;
+
+      let status = this.searchForm.value.status;
+      this.operationApiService
+        .searchOperationList(
+          code,
+          startOfMonth,
+          startOfNextMonth,
+          work_code,
+          status,
+          page
+        )
+        .subscribe(
+          (response: any) => {
+            this.total = response.total;
+            this.datasource = response.data;
+            this.pageIndex = response.pageIndex;
+            this.pageSize = response.pageSize;
+            this.length = response.length;
+
+            var operation = new Operation();
+            for (var dat of response.data) {
+              dat.operation_name = operation.getOperation(dat.work_code);
+            }
+          },
+          (error) => {
+            // handle error
+          }
+        );
+    } else {
+      let page = 1;
+      if (event != null) {
+        page = event.pageIndex + 1;
+      }
+      this.operationApiService.getOperationList(page).subscribe(
+        (response: any) => {
+          this.total = response.total;
+          this.datasource = response.data;
+          this.pageIndex = response.pageIndex;
+          this.pageSize = response.pageSize;
+          this.length = response.length;
+
+          var operation = new Operation();
+          for (var dat of response.data) {
+            dat.operation_name = operation.getOperation(dat.work_code);
+          }
+        },
+        (error) => {
+          // handle error
+        }
+      );
+    }
+    return event;
+  }
+
+  date = new FormControl(moment());
+  chosenYearHandler(normalizedYear: Moment) {
+    const ctrlValue = this.date.value;
+    ctrlValue.year(normalizedYear.year());
+    this.date.setValue(ctrlValue);
+  }
+
+  chosenMonthHandler(
+    normalizedMonth: Moment,
+    datepicker: MatDatepicker<Moment>
+  ) {
+    const ctrlValue = this.date.value;
+    ctrlValue.month(normalizedMonth.month());
+    this.date.setValue(ctrlValue);
+    datepicker.close();
+
+    this.searchForm.controls.month.setValue(this.date.value.format('MM/YYYY'));
+  }
+
+  export() {
+    let filename = '';
+    if (this.searchForm.controls.month.value == '') {
+      const today = moment();
+      filename = `${today.month() + 1}_${today.year()}`;
+    } else {
+      filename = this.searchForm.controls.month.value;
+    }
+    filename = 'BaoCao_' + filename;
+
+    const fromDate = this.date.value.add(1, 'M');
+    var startOfMonth = `1/${fromDate.month()}/${fromDate.year()}`;
+
+    var toDate = this.date.value.add(1, 'M');
+    var startOfNextMonth = `1/${toDate.month()}/${toDate.year()}`;
+    this.date.setValue(this.date.value.subtract(2, 'M'));
+    this.searchForm.controls.month.setValue(this.date.value.format('MM/YYYY'));
+
+    let code = this.searchForm.value.code;
+    let work_code = this.searchForm.value.work_code;
+    let status = this.searchForm.value.status;
+    this.operationApiService
+      .searchAllOperations(
+        code,
+        startOfMonth,
+        startOfNextMonth,
+        work_code,
+        status
+      )
+      .subscribe(
+        (response: any) => {
+          console.log(response);
+
+          let exportdatasource: any[] = [];
+          const operation = new Operation();
+          for (let response_data of response.data) {
+            let status_str = 'Hoàn thành';
+            if (response_data.status == '0') {
+              status_str = 'Chưa hoàn thành';
+            }
+            const work_str = operation.getOperation(
+              response_data.work_code
+            ).viewValue;
+
+            exportdatasource.push({
+              STT: response_data.index,
+              Tổ: response_data.group,
+              NVVH: response_data.operator,
+              'Công việc': work_str,
+              Trạm: response_data.station_code,
+              'Trạng thái': status_str,
+              'Thời gian bắt đầu': response_data.start_date,
+              'Thời gian hoàn thành': response_data.end_date,
+              'Ghi chú': response_data.note,
+            });
+          }
+
+          this.exportService.exportExcel(exportdatasource, filename);
+        },
+        (error) => {
+          // handle error
+        }
+      );
+  }
 }
